13679355d4815cc2294c8fbad7baaedf31bf2f50
ksarson <37591778+ksarson@users.noreply.github.com>
2024-01-16 22:46:31 -0500

    Page Structure + Homepage Complete

---
+++ src/homepage.js
@@ -0,0 +1,74 @@
+const aboutParagraph1 = `Welcome to Cubone Café, a charming retreat nestled in the heart of the city.
+    At Cubone Café, we celebrate the art of coffee and the joy of shared moments.
+    Our mission is to provide a haven where people can escape, connect, and relish the simple pleasures of life.`;
+
+const aboutParagraph2 = `Explore our thoughtfully crafted menu featuring premium coffee blends sourced from diverse corners of the globe.
+Each cup at Cubone Café is a journey of flavors, from the bold intensity of our espresso to the nuanced profiles of our signature blends.
+We are committed to sustainability, ensuring our coffee beans are ethically sourced, and we actively minimize our environmental footprint.`;
+
+const aboutParagraph3 = `Join us at Cubone Café, where every cup is an invitation to savor the extraordinary.
+    Immerse yourself in the perfect blend of flavors, the warm embrace of community, and the genuine connections forged over a shared love for exceptional coffee.
+    Experience Cubone Café – where the love for coffee meets the heart of the community.`;
+
+function createAbout() {
+    const about = document.createElement('about');
+    about.classList.add('homepage-content', 'about');
+
+    const aboutText = document.createElement('div');
+    aboutText.classList.add('about-text');
+    aboutText.innerHTML = `<h1>About Us</h1><br><p>${aboutParagraph1}</p><br><p>${aboutParagraph2}</p><br><p>${aboutParagraph3}</p>`;
+
+    about.appendChild(aboutText);
+
+    return about;
+}
+
+function createHours() {
+    const hours = document.createElement('hours');
+    hours.classList.add('homepage-content', 'hours');
+
+    const hoursText = document.createElement('div');
+    hoursText.classList.add('hours-text');
+    hoursText.innerHTML = `<h1>Hours of Operation</h1><br>
+    ${createDailyHoursText('Sunday', 9, 7)}<br>
+    ${createDailyHoursText('Monday', 7, 9)}<br>
+    ${createDailyHoursText('Tuesday', 7, 9)}<br>
+    ${createDailyHoursText('Wednesday', 7, 9)}<br>
+    ${createDailyHoursText('Thursday', 7, 10)}<br>
+    ${createDailyHoursText('Friday', 7, 10)}<br>
+    ${createDailyHoursText('Saturday', 8, 11)}<br>`;
+
+    hours.appendChild(hoursText);
+
+    return hours;
+}
+
+function createDailyHoursText(dayOfTheWeek, startTime, endTime) {
+    const hoursRow = `<strong>${dayOfTheWeek}</strong>: &nbsp; ${startTime}am - ${endTime}pm`;
+    return hoursRow;
+}
+
+function createImage() {
+    const homepageImage = document.createElement('image');
+    homepageImage.classList.add('homepage-content', 'homepage-image');
+
+    const image = document.createElement('img');
+    image.classList.add('image');
+    image.setAttribute('src', './img/coffeeHomepage.jpg');
+    image.setAttribute('alt', 'coffeeHomepage');
+    //image.innerHTML = `<img src='./img/coffeeHomepage.jpg' alt='coffeeHomepage'/>`;
+
+    homepageImage.appendChild(image);
+
+    return homepageImage;
+}
+
+function createHomepage() {
+    const main = document.getElementById('main');
+
+    main.appendChild(createAbout());
+    main.appendChild(createHours());
+    main.appendChild(createImage());
+}
+
+export default createHomepage;
---
+++ src/webpage.js
@@ -0,0 +1,81 @@
+import createHomepage from './homepage';
+
+function createHeader() {
+    const header = document.createElement('header');
+    header.classList.add('header');
+
+    const restaurantName = document.createElement('h1');
+    restaurantName.classList.add('restaurant-name');
+    restaurantName.textContent = 'Cubone Café';
+
+    header.appendChild(restaurantName);
+    header.appendChild(createNav());
+
+    return header;
+}
+
+function createNav() {
+    const nav = document.createElement('nav');
+    nav.classList.add('nav');
+
+    const homepageButton = document.createElement('button');
+    homepageButton.classList.add('nav-button', 'homepage-button');
+    homepageButton.textContent = 'Homepage';
+    nav.appendChild(homepageButton);
+
+    const menuButton = document.createElement('button');
+    menuButton.classList.add('nav-button', 'menu-button');
+    menuButton.textContent = 'Menu';
+    nav.appendChild(menuButton);
+
+    const contactButton = document.createElement('button');
+    contactButton.classList.add('nav-button', 'contact-button');
+    contactButton.textContent = 'Contact';
+    nav.appendChild(contactButton);
+
+    return nav;
+}
+
+function createMain() {
+    const main = document.createElement('main');
+    main.setAttribute('id', 'main');
+    main.classList.add('main');
+
+    return main;
+}
+
+function createFooter() {
+    const footer = document.createElement('footer');
+    footer.classList.add('footer');
+
+    const footerText = document.createElement('div');
+    footerText.classList.add('footer-text');
+    footerText.textContent = 'Copyright © 2024 Kolby Sarson';
+    footer.appendChild(footerText);
+
+    const githubLink = document.createElement('a');
+    githubLink.classList.add('github-icon');
+    githubLink.href = 'https://github.com/ksarson/restaurant';
+    githubLink.target = '_blank';
+
+    const githubIcon = document.createElement('i');
+    githubIcon.classList.add('fab', 'fa-github');
+
+    githubLink.appendChild(githubIcon);
+    footer.appendChild(footerText);
+    footer.appendChild(githubLink);
+
+    return footer;
+}
+
+function initializeWebsite() {
+    const content = document.getElementById('content');
+
+    content.appendChild(createHeader());
+    content.appendChild(createMain());
+    content.appendChild(createFooter());
+
+    createHomepage();
+}
+
+export default initializeWebsite;
